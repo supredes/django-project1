3b05aad8b429313294c9d451b049746878388e6a
supredes <supredes@htmail.com>
2022-03-27 03:48:17 -0300

    Novos Arquivos HEROKU

--- projetodjango/wsgi.py
+++ projetodjango/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for projeto_django project.
+WSGI config for projetodjango project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projetodjango.settings')
 
 application = get_wsgi_application()
